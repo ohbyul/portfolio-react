2f296d3ac6b05112aab3d4501aadfb11c714b6b0
ohbyul <b.oh@urbanisom.com>
2023-11-13 09:50:36 +0900

    draw 작업중

--- src/views/project/TabDraw.js
+++ src/views/project/TabDraw.js
@@ -1,63 +1,65 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const TabDraw = () => {
     const canvasRef = useRef(null)
-    const colors = document.getElementsByClassName("jsColor");
-    const range = document.getElementById("jsRange");
     const mode = document.getElementById("jsMode");
     const saveBtn = document.getElementById("jsSave");
 
-    const INITIAL_COLOR = "2c2c2c";
     const CANVAS_SIZE_W = 800;
     const CANVAS_SIZE_H = 500;
-    useEffect(()=>{
+
+    const [range, setRange] = useState(2.5)
+    
+    const colors = ['bg-black', 'bg-white', 'bg-red', 'bg-orange', 'bg-green', 'bg-sky', 'bg-blue', 'bg-navy']
+    const INITIAL_COLOR = "2c2c2c";
+    const [color, setColor] = useState(INITIAL_COLOR)
+
+    useEffect(() => {
         if (!canvasRef) return;
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-    
+
         // 캔버스 사이즈 설정
         canvas.width = CANVAS_SIZE_W
         canvas.height = CANVAS_SIZE_H
 
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, CANVAS_SIZE_W, CANVAS_SIZE_H);
-    
-        ctx.strokeStyle = INITIAL_COLOR;
-        ctx.fillStyle = INITIAL_COLOR;
-        ctx.lineWidth = 2.5;
+
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        ctx.lineWidth = range;
 
         let painting = false;
         let filling = false;
-    
+
         function stopPainting() {
             painting = false;
         }
-    
+
         function startPainting() {
             painting = true;
         }
-    
+
         function onMouseMove(event) {
             const x = event.offsetX;
             const y = event.offsetY;
             if (!painting) {
-                //console.log("creating path in" , x ,y);
                 ctx.beginPath();   //경로 생성
                 ctx.moveTo(x, y);   //선 시작 좌표
             } else {
-                //console.log("creating line in" , x ,y);
                 ctx.lineTo(x, y);   //선 끝 좌표
                 ctx.stroke();   //선 그리기
                 //ctx.closePath();  //현대미술같은 선들..
             }
-        }    
+        }
 
         function handleCanvasClick() {
             if (filling) {
                 ctx.fillRect(0, 0, CANVAS_SIZE_W, CANVAS_SIZE_H);
             }
         }
-    
+
         function handleCM(event) {
             event.preventDefault();
         }
@@ -69,20 +71,7 @@ const TabDraw = () => {
         canvas.addEventListener("click", handleCanvasClick);
         canvas.addEventListener("contextmenu", handleCM)
 
-    },[])
-    
-
-
-    // function handleColorClick(event) {
-    //     const color = event.target.style.backgroundColor;
-    //     ctx.strokeStyle = color;
-    //     ctx.fillStyle = color;
-    // }
-
-    // function handleRangeChange(event) {
-    //     const size = event.target.value;
-    //     ctx.lineWidth = size;
-    // }
+    }, [])
 
     // function handleModeClick() {
     //     if (filling === true) {
@@ -96,7 +85,7 @@ const TabDraw = () => {
     // }
 
 
-    
+
 
     // function handleSaveClick() {
     //     const image = canvas.toDataURL();
@@ -106,7 +95,7 @@ const TabDraw = () => {
     //     link.click();
     // }
 
-    
+
 
     // Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 
@@ -121,38 +110,53 @@ const TabDraw = () => {
     // if (saveBtn) {
     //     saveBtn.addEventListener("click", handleSaveClick);
     // }
+
+    const handleRange = (e) => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        const size = e.target.value;
+        setRange(size)
+        ctx.lineWidth = size;
+    }
+
+    const handleColor = (e) => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        const colorItem = e.target.style.backgroundColor;
+        console.log(e.target);
+        setColor(colorItem)
+        ctx.strokeStyle = colorItem;
+        ctx.fillStyle = colorItem;
+    }
+
     return (
         <div className="">
             <div className='tab-title'>
                 <div className='dark:text-white'>Canvas Drawing</div>
                 <div>
+                    <div className="controls_btns">
+                        <button id="jsMode">Fill</button>
+                        <button id="jsSave">Save</button>
+                    </div>
                 </div>
             </div>
             <div className='tab-content'>
 
-                <div>
-                    <canvas ref={canvasRef} class="canvas"></canvas>
+                <div className="controls">
+                    <div className="controls_colors" id="jsColors">
+                        {colors.map((item, index) => {
+                            return (<div className={`controls_color jsColor ${item}`} key={index} onClick={handleColor}></div>)
+                        })}
+                    </div>
+                    <div className="controls_range">
+                        <input type="range" id="jsRange" min="0.1" max="10.0" value={range} step="0.1" onChange={handleRange} />
+                    </div>
                 </div>
 
-                <div class="controls">
-                    <div class="controls_range">
-                        <input type="range" id="jsRange" min="0.1" max="5.0" value="2.5" step="0.1" />
-                    </div>
-                    <div class="controls_btns">
-                        <button id="jsMode">Fill</button>
-                        <button id="jsSave">Save</button>
-                    </div>
-                    <div class="controls_colors" id="jsColors">
-                        <div class="controls_color jsColor bg-black"></div>
-                        <div class="controls_color jsColor bg-white"></div>
-                        <div class="controls_color jsColor bg-red"></div>
-                        <div class="controls_color jsColor bg-orange"></div>
-                        <div class="controls_color jsColor bg-green"></div>
-                        <div class="controls_color jsColor bg-sky"></div>
-                        <div class="controls_color jsColor bg-blue"></div>
-                        <div class="controls_color jsColor bg-navy"></div>
-                    </div>
+                <div>
+                    <canvas ref={canvasRef} className="canvas"></canvas>
                 </div>
+
             </div>
 
         </div>
