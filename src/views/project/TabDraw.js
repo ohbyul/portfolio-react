d1595fc0e1c08068995e8255ee65e1b5560ead1e
ohbyul <b.oh@urbanisom.com>
2024-01-12 16:13:31 +0900

    수정

--- src/views/project/TabDraw.js
+++ src/views/project/TabDraw.js
@@ -2,17 +2,28 @@ import React, { useEffect, useRef, useState } from 'react';
 
 const TabDraw = () => {
     const canvasRef = useRef(null)
-    const mode = document.getElementById("jsMode");
-    const saveBtn = document.getElementById("jsSave");
-
     const CANVAS_SIZE_W = 800;
     const CANVAS_SIZE_H = 500;
 
-    const [range, setRange] = useState(2.5)
-    
-    const colors = ['bg-black', 'bg-white', 'bg-red', 'bg-orange', 'bg-green', 'bg-sky', 'bg-blue', 'bg-navy']
-    const INITIAL_COLOR = "2c2c2c";
+    const [range, setRange] = useState(5.0)
+
+    const [mode, setMode] = useState('paint')
+    let modeRef = useRef('paint')
+
+    const colors = [
+        { class: 'bg-black', color: '#2c2c2c', name: 'black' },
+        { class: 'bg-white', color: '#ffffff', name: 'white' },
+        { class: 'bg-red', color: '#FF3B30', name: 'red' },
+        { class: 'bg-orange', color: '#ff9500', name: 'orange' },
+        { class: 'bg-green', color: '#4cd963', name: 'green' },
+        { class: 'bg-sky', color: '#5ac8fa', name: 'sky' },
+        { class: 'bg-blue', color: '#0579ff', name: 'blue' },
+        { class: 'bg-navy', color: '#5856d6', name: 'navy' },
+    ]
+    const INITIAL_COLOR = "#2c2c2c";
     const [color, setColor] = useState(INITIAL_COLOR)
+
+    const colordata = document.getElementsByClassName("jsColor");
 
     useEffect(() => {
         if (!canvasRef) return;
@@ -23,7 +34,7 @@ const TabDraw = () => {
         canvas.width = CANVAS_SIZE_W
         canvas.height = CANVAS_SIZE_H
 
-        ctx.fillStyle = "white";
+        ctx.fillStyle = "#ffffff";
         ctx.fillRect(0, 0, CANVAS_SIZE_W, CANVAS_SIZE_H);
 
         ctx.strokeStyle = color;
@@ -31,85 +42,53 @@ const TabDraw = () => {
         ctx.lineWidth = range;
 
         let painting = false;
-        let filling = false;
-
-        function stopPainting() {
-            painting = false;
-        }
 
         function startPainting() {
-            painting = true;
-        }
-
-        function onMouseMove(event) {
-            const x = event.offsetX;
-            const y = event.offsetY;
-            if (!painting) {
-                ctx.beginPath();   //경로 생성
-                ctx.moveTo(x, y);   //선 시작 좌표
-            } else {
-                ctx.lineTo(x, y);   //선 끝 좌표
-                ctx.stroke();   //선 그리기
-                //ctx.closePath();  //현대미술같은 선들..
+            if (modeRef.current === 'paint') {
+                painting = true;
             }
-        }
-
-        function handleCanvasClick() {
-            if (filling) {
+            else {
                 ctx.fillRect(0, 0, CANVAS_SIZE_W, CANVAS_SIZE_H);
             }
         }
 
-        function handleCM(event) {
-            event.preventDefault();
+        function stopPainting() {
+            if (modeRef.current !== 'paint') return;
+            painting = false;
         }
 
-        canvas.addEventListener("mousemove", onMouseMove);
+        function onMouseMove(e) {
+            if (modeRef.current !== 'paint') return;
+            const x = e.offsetX;
+            const y = e.offsetY;
+            if (!painting) {
+                ctx.beginPath();    //경로 생성
+                ctx.moveTo(x, y);   //선 시작 좌표
+            } else {
+                ctx.lineTo(x, y);   //선 끝 좌표
+                ctx.stroke();       //선 그리기
+                // ctx.closePath();  //현대미술같은 선들..
+            }
+        }
+
+        function handleCM(e) {
+            e.preventDefault();
+        }
+
         canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("mousemove", onMouseMove);
         canvas.addEventListener("mouseup", stopPainting);
         canvas.addEventListener("mouseleave", stopPainting);
-        canvas.addEventListener("click", handleCanvasClick);
         canvas.addEventListener("contextmenu", handleCM)
 
     }, [])
 
-    // function handleModeClick() {
-    //     if (filling === true) {
-    //         filling = false;
-    //         mode.innerText = "FILL";
-    //     } else {
-    //         filling = true;
-    //         mode.innerText = "PAINT"
 
-    //     }
-    // }
+    useEffect(() => {
+        console.log("[mode]", mode);
+        modeRef.current = mode;
 
-
-
-
-    // function handleSaveClick() {
-    //     const image = canvas.toDataURL();
-    //     const link = document.createElement("a");
-    //     link.href = image;
-    //     link.download = "PaintJS[🎨]";
-    //     link.click();
-    // }
-
-
-
-    // Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
-
-    // if (range) {
-    //     range.addEventListener("input", handleRangeChange);
-    // }
-
-    // if (mode) {
-    //     mode.addEventListener("click", handleModeClick);
-    // }
-
-    // if (saveBtn) {
-    //     saveBtn.addEventListener("click", handleSaveClick);
-    // }
+    }, [mode])
 
     const handleRange = (e) => {
         const canvas = canvasRef.current;
@@ -119,14 +98,27 @@ const TabDraw = () => {
         ctx.lineWidth = size;
     }
 
-    const handleColor = (e) => {
+    const handleColor = (item) => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-        const colorItem = e.target.style.backgroundColor;
-        console.log(e.target);
+        const colorItem = item.color;
+        console.log(colorItem);
         setColor(colorItem)
         ctx.strokeStyle = colorItem;
         ctx.fillStyle = colorItem;
+    }
+
+    const handleMode = (e) => {
+        setMode(e.target.id)
+    }
+
+    const handleSaveClick = () => {
+        const canvas = canvasRef.current;
+        const image = canvas.toDataURL();
+        const link = document.createElement("a");
+        link.href = image;
+        link.download = "PaintJS[🎨]";
+        link.click();
     }
 
     return (
@@ -134,9 +126,11 @@ const TabDraw = () => {
             <div className='tab-title'>
                 <div className='dark:text-white'>Canvas Drawing</div>
                 <div>
+                    <div> color : {colors.find(x => x.color === color)?.name} </div>
                     <div className="controls_btns">
-                        <button id="jsMode">Fill</button>
-                        <button id="jsSave">Save</button>
+                        <button id="paint" className={`jsMode ${mode === 'paint' ? 'active' : ''}`} onClick={handleMode}>PAINT</button>
+                        <button id="fill" className={`jsMode ${mode === 'fill' ? 'active' : ''}`} onClick={handleMode}>FILL</button>
+                        <button id="jsSave" onClick={handleSaveClick}>Save</button>
                     </div>
                 </div>
             </div>
@@ -145,7 +139,10 @@ const TabDraw = () => {
                 <div className="controls">
                     <div className="controls_colors" id="jsColors">
                         {colors.map((item, index) => {
-                            return (<div className={`controls_color jsColor ${item}`} key={index} onClick={handleColor}></div>)
+                            return (<div className={`controls_color jsColor ${item.class} ${color === item.color ? 'active-on' : ''}`}
+                                key={index}
+                                onClick={() => handleColor(item)}></div>
+                            )
                         })}
                     </div>
                     <div className="controls_range">
