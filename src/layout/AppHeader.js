ab96c1557ebed72d5af4336214236c56002fc0d4
ohbyul <b.oh@urbanisom.com>
2024-01-23 15:51:51 +0900

    hidden 제거

--- src/layout/AppHeader.js
+++ src/layout/AppHeader.js
@@ -28,7 +28,8 @@ const AppHeader = (props) => {
                         <DarkThemeToggle />
                     </div>
 
-                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    {/* hidden 제거 */}
+                    <div className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 nav-menu">
                             {
                                 menuList?.map((menu, index) => {
